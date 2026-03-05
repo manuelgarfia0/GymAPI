109af13fd2ff88ed652729fea0fd308edbae4fda
manuelgarfia0 <manuel.garfia@iesnervion.es>
2026-03-05 16:58:04 +0100

    Nombre de la entidad Exercise, cambiado en base de datos a exercises

--- gym-api/src/main/java/com/manuel/gym_api/model/Exercise.java
+++ gym-api/src/main/java/com/manuel/gym_api/model/Exercise.java
@@ -4,31 +4,39 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "exercises")
 public class Exercise {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
 	private String muscleGroup;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getMuscleGroup() {
 		return muscleGroup;
 	}
+
 	public void setMuscleGroup(String muscleGroup) {
 		this.muscleGroup = muscleGroup;
 	}
